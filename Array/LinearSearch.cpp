4c590b3161166a1fc04f25d3c0353c72cd63fb88
vipinrao009 <vipinrao2018@gmail.com>
2023-09-14 00:10:56 +0530

    error solved

--- Array/LinearSearch.cpp
+++ Array/LinearSearch.cpp
@@ -1,40 +1,37 @@
-//Make a program for linear search using function
+// Make a program for linear search using function
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-bool linearSearch(int arr[],int size,int target)
+bool linearSearch(int arr[], int size, int target)
 {
 
-   for(int i=0; i<size; i++)
-   {
-       if(arr[i]==target)
-       {
-        //found
-        return true;
-       }
-       else
-       {
-        //not found
-        return false;
-       }
-   }
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+        {
+            // found
+            return true;
+        }
+    }
+    // not found
+    return false;
 }
 
 int main()
 {
-    int arr[6]={10,20,50,40,30,70};
-    int size =6;
-    int target = 50;
-    bool ans = linearSearch(arr,size,target);
-    
-    if(ans==true)
+    int arr[6] = {10, 20, 50, 40, 30, 70};
+    int size = 6;
+    int target = 70;
+    bool ans = linearSearch(arr, size, target);
+
+    if (ans == true)
     {
-        cout<<"Target found";
+        cout << "Target found";
     }
     else
     {
-        cout<<"Target is not found";
+        cout << "Target is not found";
     }
 
     return 0;
