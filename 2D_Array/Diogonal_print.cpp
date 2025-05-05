d154adaeff8b7e8b87eaa164aefed31ed1544a68
vipinrao009 <vipinrao2018@gmail.com>
2025-05-05 23:20:16 +0530

    Implement function to print second diagonal of a 2D array

--- 2D_Array/Diogonal_print.cpp
+++ 2D_Array/Diogonal_print.cpp
@@ -11,6 +11,15 @@ void printArray(int arr[][4], int row, int col)
     }
 }
 
+void printSecondDiagonal(int arr[][4], int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        cout << arr[i][col - 1 - i] << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
     int arr[][4] =
@@ -23,5 +32,5 @@ int main()
     int row = 4;
     int col = 4;
 
-    printArray(arr, row, col);
+    printSecondDiagonal(arr, row, col);
 }
